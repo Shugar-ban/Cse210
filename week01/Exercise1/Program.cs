c438ce882600a562e4cf9e6b861693f9f6c7a560
Shugar-ban <adeoyeayodeji913@gmail.com>
2025-08-03 06:28:31 -0400

    update

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name?");
+        Console.WriteLine("What is your last name?");
+
+        string f_name = Console.ReadLine();
+        string l_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_name},{f_name} {l_name}");
     }
 }
